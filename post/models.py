ef42885e0dff8d3f5b40ddc4ef09790fde97d7d8
vikas kushwah <rakeshsinghkushwah09@gmail.com>
2020-04-05 15:40:26 +0530

    html,login,logout ,model,view

--- post/models.py
+++ post/models.py
@@ -4,6 +4,7 @@ from django.core import validators
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
+
 def check_minlenth(val):
     if len(val) < 10:
         raise validators.ValidationError('Ti must be greter then 10')
@@ -18,6 +19,8 @@ class Category(models.Model):
         return self.title
     class Meta:
         db_table = 'categories'
+        verbose_name = 'categories'
+        verbose_name_plural = 'Categories'
 
 # Create your models here.
 class Posts(models.Model):
@@ -28,21 +31,43 @@ class Posts(models.Model):
 
     # title =   models.CharField(max_length=50,validators=[check_minlenth])
     title =   models.CharField(max_length=50)
-    user = models.OneToOneField(User,on_delete=models.CASCADE,default=1)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,default=1,null=True)
     content = models.TextField()
+    thumbnail = models.FileField(upload_to="posts/",null=True)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
     class Meta:
-        db_table = 'posts'
+        db_table = 'posts' #table name in database 
+        verbose_name = 'Posts' # Show in admin for sigular
+        verbose_name_plural = 'Posts' # shwo in admin for plurals
 
 #create a form these form will be shown on model template
 class PostForm(forms.ModelForm):
     class Meta:
         model = Posts
-        fields= ['title','content','user']
+        fields= ['title','content','thumbnail','user']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control','placeholder':'Enter email'}),
+            'content': forms.Textarea(attrs={'class': 'form-control','placeholder':'write here'}),
+            'thumbnail': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'user'  : forms.Select(attrs={'class':'form-control'}),
+        }
+        labels = {
+            'title':'Title(Enter Youer Name)'
+        }
+        help_texts = {
+            'title': ('Some useful help text.'),
+        }
+        error_messages = {
+            'title':{
+                'max_length': 'Abe 50 se jata charcter enter mat kar'
+
+            }
+        }
+        
     
     # def clean(self):
     #     fields = self.cleaned_data
--- post/views.py
+++ post/views.py
@@ -9,9 +9,9 @@ def index(request):
     form = PostForm()
     row_data = []
     if request.method == 'POST':
-        form = PostForm(request.POST)
+        form = PostForm(request.POST,request.FILES)
         if form.is_valid():
             if form.save():
                 row_data = Posts.objects.all()
-    print(form)
+    # print(form)
     return render(request,'index.html',{'form':form,'row_data':row_data})
